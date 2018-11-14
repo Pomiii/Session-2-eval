9419dc88ac4c8b48a68bc938b74f12ff0a9a2240
Balbri <nicolas.filine.lp2@gmail.com>
2018-11-14 11:49:04 +0100

    Modif Apprenant.java toString

--- Java/src/pomNico/Region.java
+++ Java/src/pomNico/Region.java
@@ -1,5 +1,28 @@
 package pomNico;
 
 public class Region {
+int idRegion;
+String nomRegion;
+public Region(int idRegion, String nomRegion) {
+	super();
+	this.idRegion = idRegion;
+	this.nomRegion = nomRegion;
+}
+public int getIdRegion() {
+	return idRegion;
+}
+public void setIdRegion(int idRegion) {
+	this.idRegion = idRegion;
+}
+public String getNomRegion() {
+	return nomRegion;
+}
+public void setNomRegion(String nomRegion) {
+	this.nomRegion = nomRegion;
+}
+@Override
+public String toString() {
+	return ""+ "Region :" + " " + idRegion + " " + nomRegion;
+}
 
 }
