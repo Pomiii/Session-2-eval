b0de43815595ab9a8e59f00a223ee317e25bd496
Balbri <nicolas.filine.lp2@gmail.com>
2018-11-14 17:28:09 +0100

    main + requetes

--- Java/src/pomNico/Main.java
+++ Java/src/pomNico/Main.java
@@ -5,18 +5,72 @@ import java.util.ArrayList;
 
 public class Main {
 	public static void main (String[] args) throws ClassNotFoundException, SQLException{
-		
+
+		//appel d'ouverture de connexion
 		ConnexionDB.connexion();
 		
+		//appeler la methode pour afficher tous les apprenants
+		tousApprenants();
+		attenteEntree();
 		
+		//appeler la methode pour afficher ceux de la region IDF
+		regionIDF();
+		attenteEntree();
+		
+		//appeler la methode pour afficher ceux de la region PL
+		regionPL();
+		attenteEntree();
+		
+		//appeler la methode pour afficher ceux de la region Aq
+		regionAq();
+		
+		//appel de fermeture de connexion
+		ConnexionDB.closeConnexion();
+	}
+
+	public static void attenteEntree() {
+		System.out.println("Appuyez sur entrée pour continuer");
+		try{       
+			System.in.read();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public static void tousApprenants() throws ClassNotFoundException, SQLException {
 		ArrayList<Apprenant> getAll = Requetes.afficherApprenantNom();
-		
 		for (Apprenant apprenant : getAll) {
+			System.out.println(apprenant.getNomApprenant() + " " + apprenant.getPrenomApprenant());
+		}
+	}
+
+	public static void regionAq() throws ClassNotFoundException, SQLException {
+		ArrayList<Apprenant> getAllbyRegionAq = Requetes.afficherApprenantAq();
+
+		for (Apprenant apprenant : getAllbyRegionAq) {
 			System.out.println(apprenant.getNomApprenant() + " \t\t" + apprenant.getPrenomApprenant());
 		}
-		
-		//ConnexionDB.closeConnexion();
 	}
-	
-	
+
+	public static void regionPL() throws ClassNotFoundException, SQLException {
+		ArrayList<Apprenant> getAllbyRegionPL = Requetes.afficherApprenantPL();
+
+		for (Apprenant apprenant : getAllbyRegionPL) {
+			System.out.println(apprenant.getNomApprenant() + " \t\t" + apprenant.getPrenomApprenant());
+		}
+		System.out.println();
+	}
+
+	public static void regionIDF() throws ClassNotFoundException, SQLException {
+		ArrayList<Apprenant> getAllbyRegionIDF = Requetes.afficherApprenantIDF();
+
+		for (Apprenant apprenant : getAllbyRegionIDF) {
+			System.out.println(apprenant.getNomApprenant() + " \t\t" + apprenant.getPrenomApprenant());
+		}
+
+		System.out.println();
+	}
+
+
 }
--- Java/src/pomNico/Requetes.java
+++ Java/src/pomNico/Requetes.java
@@ -25,10 +25,69 @@ public class Requetes {
 			
 		}
 		
-		
+		System.out.println("Nom et Prénom de tous les apprenants");
 		return apprenants;
 	}
 	
 	
+	public static ArrayList<Apprenant> afficherApprenantIDF() throws ClassNotFoundException, SQLException
+
+	{
+		ArrayList<Apprenant>  apprenants = new ArrayList<Apprenant>();
+		String requete	= "SELECT  nomApprenant, prenomApprenant FROM `apprenant` WHERE idRegion = 1";
+		ResultSet resultat = ConnexionDB.executerQuery(requete);
+		while(resultat.next())
+		{
+			
+			Apprenant apprenant = new Apprenant();
+			apprenant.setNomApprenant(resultat.getString("nomApprenant"));
+			apprenant.setPrenomApprenant(resultat.getString("prenomApprenant"));
+			apprenants.add(apprenant);
+			
+		}
+		
+		System.out.println("Liste des habitants de l'IDF :");
+		return apprenants;
+	}
+	
+	public static ArrayList<Apprenant> afficherApprenantPL() throws ClassNotFoundException, SQLException
+
+	{
+		ArrayList<Apprenant>  apprenants = new ArrayList<Apprenant>();
+		String requete	= "SELECT  nomApprenant, prenomApprenant FROM `apprenant` WHERE idRegion = 2";
+		ResultSet resultat = ConnexionDB.executerQuery(requete);
+		while(resultat.next())
+		{
+			
+			Apprenant apprenant = new Apprenant();
+			apprenant.setNomApprenant(resultat.getString("nomApprenant"));
+			apprenant.setPrenomApprenant(resultat.getString("prenomApprenant"));
+			apprenants.add(apprenant);
+			
+		}
+		
+		System.out.println("Liste des habitants du Pays de la Loire :");
+		return apprenants;
+	}
+	
+	public static ArrayList<Apprenant> afficherApprenantAq() throws ClassNotFoundException, SQLException
+
+	{
+		ArrayList<Apprenant>  apprenants = new ArrayList<Apprenant>();
+		String requete	= "SELECT  nomApprenant, prenomApprenant FROM `apprenant` WHERE idRegion = 2";
+		ResultSet resultat = ConnexionDB.executerQuery(requete);
+		while(resultat.next())
+		{
+			
+			Apprenant apprenant = new Apprenant();
+			apprenant.setNomApprenant(resultat.getString("nomApprenant"));
+			apprenant.setPrenomApprenant(resultat.getString("prenomApprenant"));
+			apprenants.add(apprenant);
+			
+		}
+		
+		System.out.println("Liste des habitants du Pays de l'Aquitaine :");
+		return apprenants;
+	}
 }
 
